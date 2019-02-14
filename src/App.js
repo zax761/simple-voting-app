889856ca19492e98877d8d2ddec6c8f4677442c9
zax761 <zax761@bu.edu>
2019-02-13 18:53:02 -0800

    Complete function of adding website

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 import thumbsUp from "./thumbs-up-solid.svg";
 import thumbsDown from "./thumbs-down-solid.svg";
+import Modal from "react-modal";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = {
+      data: [],
+      modal: {
+        isOpen: false,
+        websiteName: "",
+        websiteUrl: "",
+        websiteDescription: ""
+      }
+    };
   }
 
   componentDidMount() {
@@ -14,41 +23,42 @@ class App extends Component {
       data: [
         {
           name: "Stack Overflow",
-          path: "https://stackoverflow.com",
+          url: "https://stackoverflow.com",
           likes: 1,
           dislikes: 1,
           description: "Question Answer Website"
         },
         {
           name: "Envato",
-          path: "https://envato.com",
+          url: "https://envato.com",
           likes: 2,
           dislikes: 2,
           description: "Tuts Plus Community"
         },
         {
           name: "Google",
-          path: "https://www.google.com",
+          url: "https://www.google.com",
           likes: 3,
           dislikes: 3,
           description: "Search Engine"
         },
         {
           name: "Angular Code",
-          path: "https://www.angularcode.com",
+          url: "https://www.angularcode.com",
           likes: 4,
           dislikes: 4,
           description: "AngularJS tutorials"
         },
         {
           name: "React",
-          path: "https://reactjs.org",
+          url: "https://reactjs.org",
           likes: 5,
           dislikes: 5,
           description: "ReactJS tutorials"
         }
       ]
     });
+    Modal.setAppElement("body");
   }
 
   upVoteHandler = index => {
@@ -74,62 +84,161 @@ class App extends Component {
   };
 
   openWebsiteHandler = index => {
-    const win = window.open(this.state.data[index].path, "_blank");
+    const win = window.open(this.state.data[index].url, "_blank");
     win.focus();
   };
 
-  addWebsiteHandler = () => {
-    alert("This function will be completed later. ");
+  openModal = () => {
+    this.setState({ modal: { ...this.state.modal, isOpen: true } });
+  };
+
+  // afterOpenModal = () => {
+  //   this.subtitle.style.color = "#f00";
+  // };
+
+  closeModal = () => {
+    this.setState({ modal: { ...this.state.modal, isOpen: false } });
+  };
+
+  setWebsiteName = e => {
+    const websiteName = e.target.value;
+    this.setState({
+      modal: { ...this.state.modal, websiteName: websiteName }
+    });
+  };
+
+  setWebsiteUrl = e => {
+    const websiteUrl = e.target.value;
+    this.setState({
+      modal: { ...this.state.modal, websiteUrl: websiteUrl }
+    });
+  };
+
+  setWebsiteDescription = e => {
+    const websiteDescription = e.target.value;
+    this.setState({
+      modal: { ...this.state.modal, websiteDescription: websiteDescription }
+    });
+  };
+
+  submitWebsite = () => {
+    const { modal } = this.state;
+    if (!modal.websiteName.trim()) {
+      alert("Invalid websiteName. ");
+    } else if (!modal.websiteUrl.trim()) {
+      alert("Invalid websiteUrl. ");
+    } else if (!modal.websiteDescription.trim()) {
+      alert("Invalid websiteDescription. ");
+    } else {
+      const { data } = this.state;
+
+      this.setState({
+        data: [
+          ...data,
+          {
+            name: modal.websiteName,
+            url: modal.websiteUrl,
+            likes: 0,
+            dislikes: 0,
+            description: modal.websiteDescription
+          }
+        ]
+      });
+    }
   };
 
   render() {
-    const { data } = this.state;
+    const { data, modal } = this.state;
+    const isOpen = modal.isOpen;
     return (
       <div className="App">
         <p className="header">Please vote for your favorite website:</p>
-        <table>
-          {data.map((value, index) => (
-            <tr key={index}>
-              <td className="thumbsIconTd">
-                <img
-                  className="clickable thumbsIcon"
-                  onClick={() => this.upVoteHandler(index)}
-                  src={thumbsUp}
-                  alt="thumbsUp"
-                />
-              </td>
-              <td className="number">{value.likes}</td>
-              <td className="thumbsIconTd">
-                <img
-                  className="clickable thumbsIcon"
-                  onClick={() => this.downVoteHandler(index)}
-                  src={thumbsDown}
-                  alt="thumbsDown"
-                />
-              </td>
-              <td className="number">{value.dislikes}</td>
-              <td
-                className={`${index % 2 === 0 ? "empty" : "withcolor"} website`}
-              >
-                <p
-                  className="clickable websiteName websiteP"
-                  onClick={() => this.openWebsiteHandler(index)}
+        <table className="mainTable">
+          <tbody>
+            {data.map((value, index) => (
+              <tr key={index}>
+                <td className="thumbsIconTd">
+                  <img
+                    className="clickable thumbsIcon"
+                    onClick={() => this.upVoteHandler(index)}
+                    src={thumbsUp}
+                    alt="thumbsUp"
+                  />
+                </td>
+                <td className="number">{value.likes}</td>
+                <td className="thumbsIconTd">
+                  <img
+                    className="clickable thumbsIcon"
+                    onClick={() => this.downVoteHandler(index)}
+                    src={thumbsDown}
+                    alt="thumbsDown"
+                  />
+                </td>
+                <td className="number">{value.dislikes}</td>
+                <td
+                  className={`${
+                    index % 2 === 0 ? "empty" : "withcolor"
+                  } website`}
                 >
-                  {value.name}
-                </p>
-                <p className="websiteDescription websiteP">
-                  {value.description}
-                </p>
-              </td>
-            </tr>
-          ))}
+                  <p
+                    className="clickable websiteName websiteP"
+                    onClick={() => this.openWebsiteHandler(index)}
+                  >
+                    {value.name}
+                  </p>
+                  <p className="websiteDescription websiteP">
+                    {value.description}
+                  </p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        <button
-          className="addWebsiteButton"
-          onClick={() => this.addWebsiteHandler()}
-        >
+        <button className="clickable" onClick={this.openModal}>
           Add Your Favorite Website
         </button>
+        <Modal
+          className="modal"
+          isOpen={isOpen}
+          // onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          contentLabel="Example Modal"
+        >
+          {/* <h2 ref={subtitle => (this.subtitle = subtitle)}>Add website</h2> */}
+          <h2 className="modalTitle">Add website</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <label>
+                    Website Name
+                    <input onChange={this.setWebsiteName} />
+                  </label>
+                </td>
+                <td>
+                  <label>
+                    Website Url
+                    <input onChange={this.setWebsiteUrl} />
+                  </label>
+                </td>
+                <td>
+                  <label>
+                    Website Description
+                    <input onChange={this.setWebsiteDescription} />
+                  </label>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="modalButtons">
+            <button className="clickable" onClick={this.submitWebsite}>
+              Submit
+            </button>
+            <button className="clickable" onClick={this.closeModal}>
+              Close
+            </button>
+          </div>
+        </Modal>
       </div>
     );
   }
